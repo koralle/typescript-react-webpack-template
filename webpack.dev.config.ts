ae8c0bd1195966d440c280c4c860686335074375
koralle <mamoru.kosugiyama@ncdc.co.jp>
2020-07-23 14:41:48 +0900

    add: webpack-bundle-analyzerを導入したり

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -2,6 +2,7 @@ import { merge } from 'webpack-merge';
 import base, { outputPath } from './webpack.base.config';
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 // Webpack-dev-Serverの設定
 const devServer: WebpackDevServer.Configuration = {
@@ -16,11 +17,14 @@ const devServer: WebpackDevServer.Configuration = {
   historyApiFallback: true,
 };
 
+const devPlugins: webpack.Plugin[] = [new BundleAnalyzerPlugin()];
+
 // 開発用のWebpackの設定
 const devConfig: webpack.Configuration = merge(base, {
   mode: 'development',
   devtool: '#inline-source-map',
   devServer: devServer,
+  plugins: devPlugins,
 });
 
 export default devConfig;
